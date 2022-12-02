dab900720015fe0695ccffb898eb2ae840bdf894
Petter Charles Stellberg Redfern <predfern@microsoft.com>
2022-12-02 16:21:59 +0100

    Solve day 1

--- AdventOfCode.Solutions/Year2022/Day01/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day01/Solution.cs
@@ -2,15 +2,20 @@ namespace AdventOfCode.Solutions.Year2022.Day01;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(01, 2022, "") { }
+    private readonly List<int> elves;
+
+    public Solution() : base(01, 2022, "Calorie Counting")
+    {
+        elves = Input.SplitByParagraph().Select(elf => elf.SplitByNewline().Sum(calories => int.Parse(calories))).OrderDescending().ToList();
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        return elves[0].ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        return (elves[0] + elves[1] + elves[2]).ToString();
     }
 }
