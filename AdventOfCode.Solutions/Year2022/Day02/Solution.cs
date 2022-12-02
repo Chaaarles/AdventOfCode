e3a1d986d49850eaf686df82a6a3f62806e03108
Petter Charles Stellberg Redfern <predfern@microsoft.com>
2022-12-02 16:49:18 +0100

    Solve day 2

--- AdventOfCode.Solutions/Year2022/Day02/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day02/Solution.cs
@@ -2,15 +2,41 @@ namespace AdventOfCode.Solutions.Year2022.Day02;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(02, 2022, "") { }
+    private static readonly Dictionary<string, int> pointsPartOne = new()
+        {
+            {"A X", 4 },
+            {"A Y", 8 },
+            {"A Z", 3 },
+            {"B X", 1 },
+            {"B Y", 5 },
+            {"B Z", 9 },
+            {"C X", 7 },
+            {"C Y", 2 },
+            {"C Z", 6 },
+        };
+
+    private static readonly Dictionary<string, int> pointsPartTwo = new()
+        {
+            {"A X", 3 },
+            {"A Y", 4 },
+            {"A Z", 8 },
+            {"B X", 1 },
+            {"B Y", 5 },
+            {"B Z", 9 },
+            {"C X", 2 },
+            {"C Y", 6 },
+            {"C Z", 7 },
+        };
+
+    public Solution() : base(02, 2022, "Rock Paper Scissors") { }
 
     protected override string SolvePartOne()
     {
-        return "";
+        return Input.SplitByNewline().Aggregate(0, (current, game) => current + pointsPartOne[game]).ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        return Input.SplitByNewline().Aggregate(0, (current, game) => current + pointsPartTwo[game]).ToString();
     }
 }
