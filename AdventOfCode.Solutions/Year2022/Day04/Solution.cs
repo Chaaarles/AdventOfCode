4bb8424e2dfab0824ca9a593d9069f07e855fac5
Petter Charles Redfern <petter.charles.redfern@online.ntnu.no>
2022-12-04 18:53:38 +0000

    Solve day 4

--- AdventOfCode.Solutions/Year2022/Day04/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day04/Solution.cs
@@ -6,11 +6,45 @@ class Solution : SolutionBase
 
     protected override string SolvePartOne()
     {
-        return "";
+        int containCount = 0;
+        foreach (string pair in Input.SplitByNewline())
+        {
+            var assignments = ParseAssignments(pair);
+            if ((assignments.FirstStart <= assignments.SecondStart
+                && assignments.FirstEnd >= assignments.SecondEnd)
+                || (assignments.SecondStart <= assignments.FirstStart
+                && assignments.SecondEnd >= assignments.FirstEnd))
+            {
+                containCount++;
+            }
+        }
+
+        return containCount.ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        int overlapCount = 0;
+        foreach (string pair in Input.SplitByNewline())
+        {
+            var assignments = ParseAssignments(pair);
+            if ((assignments.FirstStart <= assignments.SecondStart
+                && assignments.FirstEnd >= assignments.SecondStart)
+                || (assignments.SecondStart <= assignments.FirstStart
+                && assignments.SecondEnd >= assignments.FirstStart))
+            {
+                overlapCount++;
+            }
+        }
+
+        return overlapCount.ToString();
+    }
+
+    private (int FirstStart, int FirstEnd, int SecondStart, int SecondEnd) ParseAssignments(string input)
+    {
+        string[] splitPair = input.Split(',');
+        int[] firstAssignment = splitPair[0].ToIntArray("-");
+        int[] secondAssignment = splitPair[1].ToIntArray("-");
+        return (firstAssignment[0], firstAssignment[1], secondAssignment[0], secondAssignment[1]);
     }
 }
