af3d8f109c5643b94dc55d69e7872a7eec63c04d
Petter Charles Redfern <petter.charles.redfern@online.ntnu.no>
2022-12-04 18:33:47 +0000

    Solve day 3

--- AdventOfCode.Solutions/Year2022/Day03/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day03/Solution.cs
@@ -6,11 +6,52 @@ class Solution : SolutionBase
 
     protected override string SolvePartOne()
     {
-        return "";
+        int prioritySum = 0;
+        foreach (string rucksack in Input.SplitByNewline())
+        {
+            var compartment1 = new HashSet<char>();
+            for (int i = 0; i < rucksack.Length / 2; i++)
+            {
+                compartment1.Add(rucksack[i]);
+            }
+
+            for (int i = rucksack.Length / 2; i < rucksack.Length; i++)
+            {
+                if (compartment1.Contains(rucksack[i]))
+                {
+                    prioritySum += GetPriority(rucksack[i]);
+                    break;
+                }
+            }
+        }
+        return prioritySum.ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        int prioritySum = 0;
+        string[] rucksacks = Input.SplitByNewline();
+        for (int i = 0; i < rucksacks.Length; i += 3)
+        {
+            var rucksack1 = new HashSet<char>(rucksacks[i]);
+            var rucksack2 = new HashSet<char>(rucksacks[i + 1]);
+            var rucksack3 = new HashSet<char>(rucksacks[i + 2]);
+
+            char item = rucksack1.Intersect(rucksack2).Intersect(rucksack3).First();
+            prioritySum += GetPriority(item);
+        }
+        return prioritySum.ToString();
+    }
+
+    private int GetPriority(char item)
+    {
+        if (item <= 90)
+        {
+            return item - 64 + 26;
+        }
+        else
+        {
+            return item - 96;
+        }
     }
 }
