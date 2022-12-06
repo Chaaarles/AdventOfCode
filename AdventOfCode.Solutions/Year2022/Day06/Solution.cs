ddd1c7742f53b5ce9e4ca985bfd38c87b31f832b
Petter Charles Stellberg Redfern <predfern@microsoft.com>
2022-12-06 07:05:52 +0100

    Solve day 6

--- AdventOfCode.Solutions/Year2022/Day06/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day06/Solution.cs
@@ -6,11 +6,32 @@ class Solution : SolutionBase
 
     protected override string SolvePartOne()
     {
-        return "";
+        return FindMarkerEnd(4);
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        return FindMarkerEnd(14);
+    }
+
+    private string FindMarkerEnd(int length)
+    {
+        int start = 0;
+        int end = 0;
+        var buffer = new HashSet<char>();
+
+        while (buffer.Count < length)
+        {
+            if (!buffer.Contains(Input[end]))
+            {
+                buffer.Add(Input[end++]);
+            }
+            else
+            {
+                buffer.Remove(Input[start++]);
+            }
+        }
+
+        return end.ToString();
     }
 }
