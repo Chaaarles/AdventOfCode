c410e0c90ac18eb03641945573fb89d6f578da70
Petter Charles Stellberg Redfern <predfern@microsoft.com>
2022-12-14 09:15:44 +0100

    Solve day 10

--- AdventOfCode.Solutions/Year2022/Day10/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day10/Solution.cs
@@ -1,16 +1,99 @@
-namespace AdventOfCode.Solutions.Year2022.Day10;
+﻿namespace AdventOfCode.Solutions.Year2022.Day10;
+
+using System.Reflection.Emit;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(10, 2022, "") { }
+    public Solution() : base(10, 2022, "Cathode-Ray Tube") { }
 
     protected override string SolvePartOne()
     {
-        return "";
-    }
+        string[] ops = Input.SplitByNewline();
+        int signalStrength = 0;
+        int pointer = 0;
+        int regX = 1;
+        bool adding = false;
+        string[] op; ;
+        for (int cycle = 1; cycle <= 220; cycle++)
+        {
+            if ((cycle % 40) == 20)
+            {
+                signalStrength += cycle * regX;
+            }
+            op = ops[pointer].Split(" ");
+            if (op[0] == "noop")
+            {
+                adding = false;
+                pointer++;
+            }
+            else if (op[0] == "addx")
+            {
+                if (!adding)
+                {
+                    adding = true;
+                    continue;
+                }
+                else
+                {
+                    adding = false;
+                    regX += int.Parse(op[1]);
+                    pointer++;
+                }
+            }
+        }
 
+        return signalStrength.ToString();
+    }
+   
     protected override string SolvePartTwo()
     {
-        return "";
+        string[] ops = Input.SplitByNewline();
+        var render = new StringBuilder();
+        int pointer = 0;
+        int regX = 1;
+        bool adding = false;
+        string[] op;
+
+        for (int cycle = 0; cycle < 240; cycle++)
+        {
+            // Draw
+            int pos = cycle % 40;
+            if (pos == 0)
+            {
+                render.AppendLine();
+            }
+            if (Math.Abs(pos - regX) <= 1)
+            {
+                render.Append('#');
+            }
+            else
+            {
+                render.Append(' ');
+            }
+
+            // Compute
+            op = ops[pointer].Split(" ");
+            if (op[0] == "noop")
+            {
+                adding = false;
+                pointer++;
+            }
+            else if (op[0] == "addx")
+            {
+                if (!adding)
+                {
+                    adding = true;
+                    continue;
+                }
+                else
+                {
+                    adding = false;
+                    regX += int.Parse(op[1]);
+                    pointer++;
+                }
+            }
+        }
+
+        return render.ToString();
     }
 }
